c66aa617019fa28bbd9130b1f75c89b3f866abce
Saurabh Kumar <skumar9876@gmail.com>
2023-03-11 07:44:26 +0000

    Fixed bugs.

--- bsuite/environments/deep_sea_mnist.py
+++ bsuite/environments/deep_sea_mnist.py
@@ -5,9 +5,13 @@ from bsuite.environments import deep_sea
 from bsuite.experiments.deep_sea import sweep
 from bsuite.utils import datasets
 
+import collections
+
 import dm_env
 from dm_env import specs
 import numpy as np
+
+ActionSpace = collections.namedtuple('action_space', ['n'])
 
 class DeepSeaMNIST(deep_sea.DeepSea):
     """MNIST Deep Sea environment to test for deep exploration."""
@@ -19,6 +23,19 @@ class DeepSeaMNIST(deep_sea.DeepSea):
                  randomize_actions: bool = True,
                  seed: Optional[int] = None,
                  mapping_seed: Optional[int] = None):
+
+        (images, labels), _ = datasets.load_mnist()
+        self._obs_shape = (28, 28, 2)
+        self.label_to_image_dict = {}
+        self.observation_space = {} 
+        
+        # TODO: Hacky stuff here to add gym env components.
+        self.observation_space['image'] = np.zeros((28, 28, 2))
+        self.action_space = ActionSpace(10)
+        
+        for label in range(10):
+            self.label_to_image_dict[label] = images[
+                labels == label][0:1]
         
         super().__init__(size=size,
                          deterministic=deterministic,
@@ -26,33 +43,36 @@ class DeepSeaMNIST(deep_sea.DeepSea):
                          randomize_actions=randomize_actions,
                          seed=seed,
                          mapping_seed=mapping_seed)
-        
-        (images, labels), _ = datasets.load_mnist()
-        self._obs_shape = (28, 28, 2)
-        self.label_to_image_dict = {}
-        
-        for label in range(10):
-           self.label_to_image_dict[label] = images[
-               labels == label][0:10]
            
     def _get_observation(self):
         obs = np.zeros(shape=self._obs_shape, dtype=np.float32)
 
         if self._row >= self._size:  # End of episode null observation
             return obs
-        
-        row_image_ind = np.random.randint(10)
-        col_image_ind = np.random.randint(10)
+
+        row_image_ind = np.random.randint(1)
+        col_image_ind = np.random.randint(1)
         
         row_obs = self.label_to_image_dict[self._row][row_image_ind]
-        col_obs = self.label_to_image_dict[self._col][col_image_ind]
+        col_obs = self.label_to_image_dict[self._column][col_image_ind]
         
         obs = np.concatenate(
             (row_obs[:, :, None], col_obs[:, :, None]), 
             axis=-1)
         
-        return obs
+        return {'image': obs}
     
     def observation_spec(self):
         return specs.Array(
-        shape=self._image_shape, dtype=np.float32, name='observation')
+            shape=self._obs_shape, dtype=np.float32, name='observation')
+    
+    def _reset(self) -> dm_env.TimeStep:
+        x = super()._reset()
+        return x.observation
+    
+    def _step(self, action: int) -> dm_env.TimeStep:
+        x = super()._step(action)
+        done = False
+        if self._row == self._size:
+            done = True
+        return x.observation, x.reward, done, None
